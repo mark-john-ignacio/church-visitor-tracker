2d778ae98a234abbbbd35a18703d731f6722eaeb
mark <markjohn.ignacio@cvsu.edu.ph>
2025-05-16 08:23:15 +0800

    fix: update sidebar link to point to the home page instead of dashboard

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -27,7 +27,7 @@ export function AppSidebar() {
                 <SidebarMenu>
                     <SidebarMenuItem>
                         <SidebarMenuButton size="lg" asChild>
-                            <Link href="/dashboard" prefetch>
+                            <Link href="/" prefetch>
                                 {isCollapsed ? (
                                     <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
                                         <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
